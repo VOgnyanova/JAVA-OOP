937fad1305edd3ccfaa89ae3f1b66b775f66fa6d
Victoria Ognyanova <103129757+VOgnyanova@users.noreply.github.com>
2023-03-08 13:11:38 +0200

    Polymorphism - Exercises

---
+++ _10_Polymorphism - Exercises/vehicle_extended/Bus.java
@@ -0,0 +1,24 @@
+package vehicle_extended;
+
+public class Bus extends Vehicle{
+
+    public static final double AC_ADDITIONAL_CONSUMPTION = 1.4;
+    public boolean isEmpty;
+
+    public Bus(double fuelQuantity, double fuelConsumption, double tankCapacity) {
+        super(fuelQuantity, fuelConsumption, tankCapacity);
+    }
+
+    @Override
+    public String drive(double distance){
+        setFuelConsumption(getFuelConsumption() + AC_ADDITIONAL_CONSUMPTION);
+        String driveResult =  super.drive(distance);
+        setFuelConsumption(getFuelConsumption() - AC_ADDITIONAL_CONSUMPTION);
+        return driveResult;
+    }
+
+    public String driveEmpty(double distance){
+       return super.drive(distance);
+    }
+
+}
